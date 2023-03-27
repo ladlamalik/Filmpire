dc946bbb32a575d1fe99a89cb51311c93d791117
Nadeem <ladlamalik.haxor@gmail.com>
2023-03-27 11:55:23 +0500

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const app = () => <div>App</div>;
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Reach from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('root'))
